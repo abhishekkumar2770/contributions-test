417b70115852449471c04249fb80fdbbf2dc0d59
Abhishek Kumar <abhishekk27033@gmail.com>
2026-08-08 22:59:47 +0530

    1 ms (76.35%) 11.1 MB (72.22%) 08-08-2026 0071-simplify-path Medium .cpp String

---
+++ C++/Medium/0071-simplify-path/0071-simplify-path-08-08-2026-22-59-47.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        string curr;
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (curr == "" || curr == ".") {
+                    // Ignore empty parts and "."
+                }
+                else if (curr == "..") {
+                    if (!st.empty())
+                        st.pop_back();
+                }
+                else {
+                    st.push_back(curr);
+                }
+
+                curr = "";
+            }
+            else {
+                curr += path[i];
+            }
+        }
+
+        string ans = "";
+
+        for (string &dir : st) {
+            ans += "/" + dir;
+        }
+
+        return ans.empty() ? "/" : ans;
+    }
+};
